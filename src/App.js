5ab89e5e206f3eba829e22e732b337615021dc78
daniel devine <daniel.devine@monterosa.co.uk>
2023-03-01 08:54:42 +0000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+  import { useLayoutEffect, useEffect } from 'react'
+  import { configure } from '@monterosa-sdk/core';
+  import {
+    embed,
+    unmount,
+    requestMoreData,
+  } from "@monterosa-sdk/launcher-kit";
+  import { getExperience } from '@monterosa-sdk/launcher-kit';
+  import './App.css';
 
-function App() {
+const App = () => {
+configure({ host: 'cdn-dev.monterosa.cloud', projectId: '2563de4f-422e-4b77-89df-487b6ccd4e44' });
+
+const experience = getExperience({
+  experienceUrl: '//localhost:3000/',
+  autoresizesHeight: true
+})
+
+useLayoutEffect(() => {
+  if (experience === undefined) return () => {};
+  embed(experience, "container-id");
+
+  return () => {
+    unmount("container-id");
+  };
+}, [experience]);
+
+useEffect(() => {
+  let requestCooldown = false;
+
+  const handleScroll = () => {
+    const pageHeight = document.body.scrollHeight;
+    const windowHeight = window.innerHeight;
+    const scrollPosition = window.scrollY + windowHeight + 20;
+    
+    if (!requestCooldown && scrollPosition >= pageHeight) {
+      requestCooldown = true;
+      requestMoreData(experience);
+      
+      setTimeout(() => {
+        requestCooldown = false;
+      }, 1000);
+    }
+  }
+
+  window.addEventListener('scroll', handleScroll);
+  return () => {
+    window.removeEventListener('scroll', handleScroll);
+  };
+}, [experience]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="container-id" />
     </div>
   );
 }
